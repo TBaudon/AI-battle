77f1f37709676349200cb08b2368cf5dad09f076
Thomas BAUDON <baudon.thomas@gmail.com>
2015-06-25 16:58:58 +0200

    pusblish

--- publish.bat
+++ publish.bat
@@ -1,6 +1,6 @@
-cp server ..\..\radstar
-cp Export\flash\bin\ ..\..\radstar\games\AI-battle
-git --git-dir ..\..\radstar
+cp server ../../radstar
+cp Export/flash/bin/ ../../radstar/games/AI-battle
+git --git-dir ../../radstar
 git add .
 git commit -m "pusblish"
 git push origin master
